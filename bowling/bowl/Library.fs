89c2ff0dd97544754d9db236aef51ce064790d3f
Warwick Stone <uozu.aho@gmail.com>
2023-03-24 18:57:27 +1100

    bowling: extract next2Rolls

--- bowling/bowl/Library.fs
+++ bowling/bowl/Library.fs
@@ -7,19 +7,19 @@ module Bowling =
         | Strike
         | Last of int * int * int
 
-    // let next2Rolls rest =
-    //     match rest with
-    //     | [] -> failwith "no!"
-    //     | Normal (a,b) :: rest -> a,b
-    //     | Spare a :: rest -> a, 10 - a
-    //     | Last (a,b,c) :: rest -> a,b
-    //     | Strike :: rest ->
-    //         match rest with
-    //         | [] -> failwith "Npoo"
-    //         | Normal (a,b) :: rest -> 10, a
-    //         | Spare a :: rest -> 10, a
-    //         | Strike :: rest -> 10, 10
-    //         | Last (a,b,c) :: rest -> 10, a
+    let next2Rolls rest =
+        match rest with
+        | [] -> failwith "no!"
+        | Normal (a,b) :: rest -> a,b
+        | Spare a :: rest -> a, 10 - a
+        | Last (a,b,c) :: rest -> a,b
+        | Strike :: rest ->
+            match rest with
+            | [] -> failwith "Npoo"
+            | Normal (a,b) :: rest -> 10, a
+            | Spare a :: rest -> 10, a
+            | Strike :: rest -> 10, 10
+            | Last (a,b,c) :: rest -> 10, a
 
     let scoreFrame frame rest =
         let baseScore =
@@ -33,27 +33,11 @@ module Bowling =
             | Normal _ -> 0
             | Last _ -> 0
             | Spare _ ->
-                match rest with
-                | [] -> failwith "last frame can't be a spare"
-                | next :: rest ->
-                    match next with
-                    | Normal (a, _) -> a
-                    | Last (a,_,_) -> a
-                    | Spare a -> a
-                    | Strike -> 10
+                let a, _ = next2Rolls rest
+                a
             | Strike ->
-                match rest with
-                | [] -> failwith "last frame can't be a strike"
-                | Normal (a,b) :: _ -> a + b
-                | Last (a,b,c) :: _ -> a + b
-                | Spare _ :: _ -> 10
-                | Strike :: rest -> 10 +
-                    match rest with
-                    | [] -> failwith "last frame can't be a strike"
-                    | Normal (a,_)::_ -> a
-                    | Last (a,_,_)::_ -> a
-                    | Spare a::_ -> a
-                    | Strike::_ -> 10
+                let a, b = next2Rolls rest
+                a + b
         baseScore + additionalScore
 
     let rec scoreFrames (frames: Frame list) =
