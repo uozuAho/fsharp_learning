7afb05d5d13a02fd87baab015af9710901263593
Warwick Stone <warwick_stone@fastmail.com>
2024-03-24 17:22:16 +1100

    tests

--- range/range.test/Tests.fs
+++ range/range.test/Tests.fs
@@ -4,10 +4,15 @@ open Xunit
 open range.MyRange
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
-
 let ``[2,6) contains {2,4}`` () =
     let range = Range(Closed 2, Open 6)
     Assert.True(contains range 2)
     Assert.True(contains range 4)
+
+[<Fact>]
+let ``[2,6) doesn’t contain {-1,1,6,10}`` () =
+    let range = Range(Closed 2, Open 6)
+    Assert.False(contains range -1)
+    Assert.False(contains range 1)
+    Assert.False(contains range 6)
+    Assert.False(contains range 10)
