23546411c52553fad525c6a7594be7c9c274c4ac
Warwick Stone <warwick_stone@fastmail.com>
2024-03-24 18:07:15 +1100

    add Range.create

--- range/range.test/Tests.fs
+++ range/range.test/Tests.fs
@@ -6,13 +6,13 @@ open range.MyRange
 
 [<Fact>]
 let ``[2,6) contains {2,4}`` () =
-    let range = Range(Closed 2, Open 6)
+    let range = Range.create(Closed 2, Open 6)
     Assert.True(contains range 2)
     Assert.True(contains range 4)
 
 [<Fact>]
 let ``[2,6) doesn’t contain {-1,1,6,10}`` () =
-    let range = Range(Closed 2, Open 6)
+    let range = Range.create(Closed 2, Open 6)
     Assert.False(contains range -1)
     Assert.False(contains range 1)
     Assert.False(contains range 6)
@@ -20,5 +20,5 @@ let ``[2,6) doesn’t contain {-1,1,6,10}`` () =
 
 [<Property>]
 let ``[a,b) contains a`` (a: int, b: int) =
-    let range = Range(Closed a, Open b)
+    let range = Range.create(Closed a, Open b)
     contains range a
--- range/range/MyRange.fs
+++ range/range/MyRange.fs
@@ -5,11 +5,15 @@ module MyRange =
         | Open of int
         | Closed of int
 
-    type Range = Endpoint * Endpoint
+    type Range private (a:Endpoint, b:Endpoint) =
+        member this.a = a
+        member this.b = b
+        static member create(a, b) = Range(a, b)
 
-    let contains range value =
-        let (start, stop) = range
-        match start, stop with
+
+    let contains (range:Range) value =
+        let a, b = range.a, range.b
+        match a, b with
         | Open s, Open e -> s < value && value < e
         | Open s, Closed e -> s < value && value <= e
         | Closed s, Open e -> s <= value && value < e
