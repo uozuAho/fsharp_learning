e302a99bd629956d5e9b4e7681de2a7645ef4aed
Warwick Stone <warwick_stone@fastmail.com>
2024-05-13 21:18:55 +1000

    add failing test: insert, return return

--- vending_machine/vend.test/VendingMachineImplTests.fs
+++ vending_machine/vend.test/VendingMachineImplTests.fs
@@ -10,8 +10,15 @@ let chooseFromList xs =
   gen { let! i = Gen.choose (0, List.length xs-1) 
         return List.item i xs }
 
+[<Fact>]
+let ``Coin return returns nothing when no coins entered`` () =
+    let api = VendingMachineImpl.api
+    let machine = api.create
+    let machine, coins = api.coinReturn machine
+    Assert.Empty coins
+
 [<Property>]
-let ``Get same coin back`` () =
+let ``Coin return: Get same coin back`` () =
     let n = chooseFromList [Nickel; Dime; Quarter; Dollar] |> Arb.fromGen
     Prop.forAll n (fun coin ->
         let api = VendingMachineImpl.api
@@ -21,3 +28,12 @@ let ``Get same coin back`` () =
         Assert.Equal(1, coins.Length)
         Assert.Equal(coin, coins.Head)
     )
+
+[<Fact>]
+let ``Coin return returns nothing after coins returned`` () =
+    let api = VendingMachineImpl.api
+    let machine = api.create
+    let machine = api.insertMoney machine Dollar
+    let machine, coins = api.coinReturn machine
+    let machine, coins = api.coinReturn machine
+    Assert.Empty coins
