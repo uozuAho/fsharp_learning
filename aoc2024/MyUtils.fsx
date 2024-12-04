31cc67f2b6f3a0214f67c031ed4d7614652d1b87
me <me@me.com>
2024-12-04 21:41:54 +1100

    refactor for easier stdin/interactive

--- aoc2024/MyUtils.fsx
+++ aoc2024/MyUtils.fsx
@@ -3,3 +3,7 @@ open System
 let readStdin =
     Seq.initInfinite (fun _ -> Console.In.ReadLine())
     |> Seq.takeWhile ((<>) null)
+
+// not sure if this is 100% correct, but it works
+let isStdInFromPipe =
+    Console.IsInputRedirected
--- aoc2024/aoc1.fsx
+++ aoc2024/aoc1.fsx
@@ -1,10 +1,18 @@
 (*
 https://adventofcode.com/2024/day/1
 
-In short:
+Part 1:
 - input = lines of pairs of numbers
 - find the total distance between the sorted number
   pairs, ie sum(abs(a - b)) for (a, b) in lines
+
+Part 2:
+- same input
+- find sum(x * num_appearances_in_right_list(x))
+
+Usage:
+xclip -o | dotnet fsi aoc1.fsx
+dotnet fsi --use:aoc1.fsx
 *)
 
 #load "MyUtils.fsx"
@@ -21,20 +29,49 @@ let line2pair (line:string) =
     | [a; b] -> Some(a, b)
     | _ -> None
 
-let input2pairs =
-    readStdin
+let parsePairs lines =
+    lines
     |> Seq.where (fun x -> not (String.IsNullOrEmpty x))
     |> Seq.map line2pair
     |> Seq.choose id
 
-let pairs = input2pairs |> Seq.toList
-let left = pairs |> Seq.map fst |> Seq.sort;
-let right = pairs |> Seq.map snd |> Seq.sort;
+let stdin2pairs stdin =
+    stdin |> parsePairs |> Seq.toList
 
-let answer =
+let string2pairs (s:string) =
+    s.Split('\n') |> parsePairs |> Seq.toList
+
+let pairs2cols pairs =
+    (pairs |> Seq.map fst,
+    pairs |> Seq.map snd)
+
+let totalDistance pairs =
+    let left, right = pairs2cols pairs
+    let left = left |> Seq.sort
+    let right = right |> Seq.sort
+
     Seq.zip left right
     |> Seq.map (fun x -> fst x - snd x)
     |> Seq.map abs
     |> Seq.sum
 
-printfn "%d" answer
+let count x list =
+    list
+    |> Seq.map (fun y -> if y = x then 1 else 0)
+    |> Seq.sum
+
+let similarityScore pairs = 3
+
+let solve pairs =
+    printfn "distance:   %d" (totalDistance pairs)
+    printfn "similarity: %d" (similarityScore pairs)
+
+let solveIfStdin =
+    match isStdInFromPipe with
+    | true ->
+        printfn "FROM STDIN:"
+        let pairs = readStdin |> parsePairs
+        solve pairs
+    | _ -> ()
+
+solveIfStdin
