e320ae346eaeb79dadab44b63529620e53f1b2ae
wozza <warwick_stone@fastmail.com>
2024-12-06 20:15:20 +1100

    WIP aoc2 part 2

--- aoc2024/aoc2.fsx
+++ aoc2024/aoc2.fsx
@@ -14,10 +14,14 @@ input: one "levels" report per line:
 Report is safe if:
 - levels are all increasing or all decreasing
 - adjacent levels differ by 1 <= x <= 3
+
+Part 2
+Report is now safe if:
+- safe as per previous rules
+- removing any one level from the report makes it safe
 *)
 
 type Report = list<int>
-type Trend = Increasing | Decreasing | NoChange | Unknown
 
 let parseReport (line:string): Report =
     line.Split(' ')
@@ -29,12 +33,13 @@ let str2reports (str:string) =
     str.Split('\n')
     |> Seq.map parseReport
 
+let ismono x y z =
+    match x, y, z with
+    | _ when x - y > 0 && y - z < 0 -> false
+    | _ when x - y < 0 && y - z > 0 -> false
+    | _ -> true
+
 let rec isMonotonic report =
-    let ismono x y z =
-        match x, y, z with
-        | _ when x - y > 0 && y - z < 0 -> false
-        | _ when x - y < 0 && y - z > 0 -> false
-        | _ -> true
     match report with
     | x::y::z::[] -> ismono x y z
     | x::y::z::rest ->
@@ -57,19 +62,42 @@ let rec allWithinBounds report =
 let isSafe report =
     isMonotonic report && allWithinBounds report
 
+// todo: need to allow one bad level
+let rec isSafePart2 report =
+    let isPartSafe x y z =
+        ismono x y z && withinBounds x y && withinBounds y z
+
+    match report with
+    | [] | [_] -> true
+    | [x; y] -> withinBounds x y
+    | x::y::z::[] -> isPartSafe x y z
+    | x::y::z::rest ->
+        if not (isPartSafe x y z) then false
+        else isSafePart2 ([y;z] @ rest)
+
 let reports =
     File.ReadLines("input.txt")
     |> Seq.map parseReport
     |> Seq.toList
 
-reports |> Seq.iter (fun report ->
-    printfn "%b: %A" (isSafe report) report
-)
+let printAllChecks check reports =
+    reports |> Seq.iter (fun report ->
+        printfn "%b: %A" (check report) report
+    )
 
-let numSafe =
+let numSafePart1 =
     reports
     |> Seq.map isSafe
     |> Seq.map (fun b -> if b then 1 else 0)
     |> Seq.sum
 
-printfn "num safe: %d" numSafe
+let numSafePart2 =
+    reports
+    |> Seq.map isSafePart2
+    |> Seq.map (fun b -> if b then 1 else 0)
+    |> Seq.sum
+
+// printAllChecks isSafe reports
+printAllChecks isSafePart2 reports
+printfn "num safe 1: %d" numSafePart1
+printfn "num safe 2: %d" numSafePart2
